72d170490315b62ff31d0e767b611d33a0ebcdd0
tigranFTP <tigranftp@gmail.com>
2023-06-01 11:54:58 +0300

    added icon

--- src/main/java/com/example/notebook/HelloApplication.java
+++ src/main/java/com/example/notebook/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.notebook;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         HelloController cntrl = fxmlLoader.getController();
         stage.setTitle("Notebook");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setScene(scene);
         stage.setOnCloseRequest(e -> {
